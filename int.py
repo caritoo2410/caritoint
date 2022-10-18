ff9a83dc30eb650a83d3c04ce3b6da55bc70da60
caritoo2410 <116046555+caritoo2410@users.noreply.github.com>
2022-10-17 20:33:48 -0500

    INTERVIEW

---
+++ int.py
@@ -0,0 +1,7 @@
+#Test json get on a URL
+import urllib.request, json
+
+
+with urllib.request.urlopen("https://62433a7fd126926d0c5d296b.mockapi.io/api/v1/usuarios") as url:
+    data = json.load(url)
+    print(data)
